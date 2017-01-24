33af265fe18cf015a34641e880291979d38f3a0f
jtc6 <jthomascullen@gmail.com>
2017-01-24 13:42:18 -0500

    Initial commit
    Opens Ipcam

---
+++ src/it/polito/elite/teaching/cv/FXHelloCVController.java
@@ -0,0 +1,181 @@
+package it.polito.elite.teaching.cv;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.highgui.VideoCapture;
+
+import it.polito.elite.teaching.cv.utils.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * The controller for our application, where the application logic is
+ * implemented. It handles the button for starting/stopping the camera and the
+ * acquired video stream.
+ */
+public class FXHelloCVController
+{
+	// the FXML button
+	@FXML
+	private Button button;
+	// the FXML image view
+	@FXML
+	private ImageView currentFrame;
+	
+	// a timer for acquiring the video stream
+	private ScheduledExecutorService timer;
+	// Does two things: Creates a videocatpure instance and designates it "capture".
+	private VideoCapture capture = new VideoCapture();
+	// a flag to change the button behavior
+	private boolean cameraActive = false;
+	// the id of the camera to be used
+	
+	/**
+	 * The action triggered by pushing the button on the GUI
+	 *
+	 * @param event
+	 *            the push button event
+	 */
+	@FXML
+	protected void startCamera(ActionEvent event)
+	{
+		if (!this.cameraActive)
+		{
+			// start the video capture
+//			this.capture.open("C:/test/test-mjpeg.mov");
+			this.capture.open("http://10.2.23.26/mjpg/video.mjpg");
+			
+			
+			// is the video stream available?
+			if (this.capture.isOpened())
+			{
+				this.cameraActive = true;
+				
+				// grab a frame every 33 ms (30 frames/sec)
+				Runnable frameGrabber = new Runnable() {
+					
+					@Override
+					public void run()
+					{
+						// effectively grab and process a single frame
+						Mat frame = grabFrame();
+						// convert and show the frame
+						Image imageToShow = Utils.mat2Image(frame);
+						updateImageView(currentFrame, imageToShow);
+					}
+				};
+				
+				this.timer = Executors.newSingleThreadScheduledExecutor();
+				this.timer.scheduleAtFixedRate(frameGrabber, 0, 50, TimeUnit.MILLISECONDS);
+				
+				// update the button content
+				this.button.setText("Stop Camera");
+			}
+			else
+			{
+				// log the error
+				System.err.println("Camera is unreachable");
+			}
+		}
+		else
+		{
+			// the camera is not active at this point
+			this.cameraActive = false;
+			// update again the button content
+			this.button.setText("Start Camera");
+			
+			// stop the timer
+			this.stopAcquisition();
+		}
+	}
+	
+	/**
+	 * Get a frame from the opened video stream (if any)
+	 *
+	 * @return the {@link Mat} to show
+	 */
+	private Mat grabFrame()
+	{
+		// init everything
+		Mat frame = new Mat();
+		
+		// check if the capture is open
+		if (this.capture.isOpened())
+		{
+			try
+			{
+				// read the current frame
+				this.capture.read(frame);
+				
+				// if the frame is not empty, process it- I TOOK THIS OUT BECAUSE I DO NOT WANT BLACK AND WHITE VIDEO
+				// im planning on adding an if statement here, as in the example.
+				// the imgproc refers to what is to be done with the image, mat refers to the scorce image, mat1 refers to the destination image, or the imageview in the FX code, and the next imgproc states more specifically what is to be done to the image.
+				//Imgproc.cvtColor(Mat, mat1 Imgproc.COLOR_RGB2GRAY);
+				
+			}
+			catch (Exception e)
+			{
+				// log the error
+				System.err.println("Exception during the image elaboration: " + e);
+			}
+		}
+		
+		return frame;
+	}
+	
+	/**
+	 * Stop the acquisition from the camera and release all the resources
+	 */
+	private void stopAcquisition()
+	{
+		if (this.timer!=null && !this.timer.isShutdown())
+		{
+			try
+			{
+				// stop the timer
+				this.timer.shutdown();
+				this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+			}
+			catch (InterruptedException e)
+			{
+				// log any exception
+				System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+			}
+		}
+		
+		if (this.capture.isOpened())
+		{
+			// release the camera
+			this.capture.release();
+		}
+	}
+	
+	/**
+	 * Update the {@link ImageView} in the JavaFX main thread
+	 * 
+	 * @param view
+	 *            the {@link ImageView} to update
+	 * @param image
+	 *            the {@link Image} to show
+	 */
+	private void updateImageView(ImageView view, Image image)
+	{
+		Utils.onFXThread(view.imageProperty(), image);
+	}
+	
+	/**
+	 * On application close, stop the acquisition from the camera
+	 */
+	protected void setClosed()
+	{
+		this.stopAcquisition();
+	}
+	
+}
